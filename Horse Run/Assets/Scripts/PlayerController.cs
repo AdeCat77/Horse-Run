026f86d0a1cbccc1cb894974fedc54757e41c8c9
SaKu0505 <sara.kurto@stud.edubs.ch>
2024-10-16 22:26:11 +0200

    PlayerControllerScript bearbeitet

--- Horse Run/Assets/Scripts/PlayerController.cs
+++ Horse Run/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+        }
     }
 }
