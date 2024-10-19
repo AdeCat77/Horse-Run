cb7900676960636d8ca01117572793baf6e99454
SaKu0505 <sara.kurto@stud.edubs.ch>
2024-10-19 23:00:14 +0200

    SpawnManagerScript edited

--- Horse Run/Assets/Scripts/SpawnManager.cs
+++ Horse Run/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private Vector3 spawnPos = new Vector3(20,0,0);
+    public GameObject obstaclePrefab;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
