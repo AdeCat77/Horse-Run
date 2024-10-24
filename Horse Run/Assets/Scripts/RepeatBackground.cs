491812e6f66d690e972e4d7fef396902620ca110
AdeCat77 <adela.kurto@stud.edubs.ch>
2024-10-24 11:02:25 +0200

    Scripts renamed: RepeatGround->RepeatBackground & MoveBack->MoveLeft
    changes in both Scripts

--- Horse Run/Assets/Scripts/RepeatBackground.cs
+++ Horse Run/Assets/Scripts/RepeatBackground.cs
@@ -10,14 +10,14 @@ public class RepeatBackground : MonoBehaviour
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.z / 0.5f;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < startPos.z - repeatWidth)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
