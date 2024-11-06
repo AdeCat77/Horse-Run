7786cb9ea8e18775b5c5509b4dcfc0b782780df2
SaKu0505 <sara.kurto@stud.edubs.ch>
2024-11-07 00:37:48 +0100

    Button position edited and tryed to rotate target

--- Horse Run/Assets/Scripts/Target.cs
+++ Horse Run/Assets/Scripts/Target.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    public float torque;
+    //private int targetRange;
+    //private float speed = 7;
+    //private float maxTorque = 3;
+    //private float yRange = 5;
+    //private float xSpawnPos = 25;
+    //public float torque;
     private GameManager gameManager;
     public int point = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        targetRb = GetComponent<Rigidbody>(); 
+        targetRb = GetComponent<Rigidbody>();
+        //targetRb.AddForce(Force(), ForceMode.Impulse);
+        //targetRb.AddTorque(Rotate(), Rotate(), Rotate(), ForceMode.Impulse);
+        //int targetRange = Random.Range(0,5);
+        //transform.position = Position();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float turn = Input.GetAxis("Horizontal");
-        targetRb.AddTorque(transform.up * torque * turn);  //Drehungen der Sammelobjekte
+        //float turn = Input.GetAxis("Horizontal");
+        //targetRb.AddTorque(transform.up * torque * turn, ForceMode.Acceleration);  //Drehungen der Sammelobjekte
+        //transform.Rotate(Vector3.up * Time.deltaTime * 5);
     }
     void OnTriggerEnter(Collider other)
     {
@@ -32,9 +42,19 @@ public class Target : MonoBehaviour
         }
     }
 
-    //void Rotate()
+    //Vector3 Force()
     //{
-        
+        //return Vector3.forward * speed;
+    //}
+
+    //float Rotate()
+    //{
+        //return Random.Range(-maxTorque, maxTorque);
+    //}
+
+    //Vector3 Position()
+    //{
+        //return new Vector3(xSpawnPos, Random.Range(0, yRange));
     //}
    
 }
