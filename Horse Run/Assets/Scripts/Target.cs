a6c15dc6f9c9ca66508247dcbebeafaa1c6da5ed
SaKu0505 <sara.kurto@stud.edubs.ch>
2024-11-09 23:59:53 +0100

    some little changes

--- Horse Run/Assets/Scripts/Target.cs
+++ Horse Run/Assets/Scripts/Target.cs
@@ -7,14 +7,8 @@ public class Target : MonoBehaviour
     private Rigidbody targetRb;
     public AudioClip collectSound;
     private AudioSource playerAudio;
-    //private int targetRange;
-    //private float speed = 7;
-    //private float maxTorque = 3;
-    //private float yRange = 5;
-    //private float xSpawnPos = 25;
-    //public float torque;
     private GameManager gameManager;
-    private PlayerController playerController;
+    Vector3 pivotPoint = new Vector3(25,5,0);
     public int point = 1;
     //public ParticleSystem explosionParticle;
     
@@ -22,22 +16,16 @@ public class Target : MonoBehaviour
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        //targetRb.AddForce(Force(), ForceMode.Impulse);
-        //targetRb.AddTorque(Rotate(), Rotate(), Rotate(), ForceMode.Impulse);
-        //int targetRange = Random.Range(0,5);
-        //transform.position = Position();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float turn = Input.GetAxis("Horizontal");
-        //targetRb.AddTorque(transform.up * torque * turn, ForceMode.Acceleration);  //Drehungen der Sammelobjekte
-        //transform.Rotate(Vector3.up * Time.deltaTime * 5);
+         
     }
+
     void OnTriggerEnter(Collider other)
     {
         Destroy(targetRb);
@@ -52,22 +40,11 @@ public class Target : MonoBehaviour
             gameManager.GameOver();
         }
     }
-   
 
-
-    //Vector3 Force()
-    //{
-        //return Vector3.forward * speed;
-    //}
-
-    //float Rotate()
-    //{
-        //return Random.Range(-maxTorque, maxTorque);
-    //}
-
-    //Vector3 Position()
-    //{
-        //return new Vector3(xSpawnPos, Random.Range(0, yRange));
-    //}
+    void RotateAroundPoint() //Drehungen der Sammelobjekte
+    {
+        //rotates around the pivot point and the Y-Axis by 360 degrees
+        transform.RotateAround(pivotPoint, Vector3.up, 360);
+    }
    
 }
